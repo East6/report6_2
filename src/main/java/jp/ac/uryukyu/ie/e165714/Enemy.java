7a2d844de33a13ef0fa7da2e5213374d506b23cb
Higashi Yonamine <e165714@ie.u-ryukyu.ac.jp>
2017-01-12 17:29:53 +0900

    ステップ3:リファクタリングしてみよう。

--- src/main/java/jp/ac/uryukyu/ie/e165714/Enemy.java
+++ src/main/java/jp/ac/uryukyu/ie/e165714/Enemy.java
@@ -4,6 +4,35 @@ package jp.ac.uryukyu.ie.e165714;
  * Created by yonaminehigashi on 2017/01/10.
  */
 public class Enemy extends LivingThing{
+    //オーバライド開始
+    @Override
+    public void attack(LivingThing opponent){
+        boolean critical;
+        if (isDead()==false) {
+            int damage = (int) (Math.random() * getAttack());
+
+            //クリティカルダメージのため
+            if( Math.random()<0.2){
+                damage=damage*2;
+                critical=true;
+            }else{
+                critical=false;
+            }
+
+            if(damage==0){
+                System.out.printf("%sの攻撃！,,,だが、%sは攻撃を回避した！\n",getName(), opponent.getName(), damage);
+            }else if(critical==true) {
+                System.out.printf("%sの攻撃！痛恨の一撃！！%sに%dのダメージを与えた！！\n", getName(), opponent.getName(), damage);
+            }else{
+                System.out.printf("%sの攻撃！%sに%dのダメージを与えた！！\n",getName(),opponent.getName(),damage);
+            }
+            opponent.wounded(damage);
+        }
+    }
+    //@Override
+
+    //オーバライド終了
+
     public Enemy(String name, int hitPoint,int attack){
         super(name,hitPoint,attack);
     }
--- src/main/java/jp/ac/uryukyu/ie/e165714/Hero.java
+++ src/main/java/jp/ac/uryukyu/ie/e165714/Hero.java
@@ -3,8 +3,35 @@ package jp.ac.uryukyu.ie.e165714;
 /**
  * Created by yonaminehigashi on 2017/01/10.
  */
+
 public class Hero extends LivingThing{
+    //Overrideはじめ
+    @Override
+    public void attack(LivingThing opponent){
+        boolean critical;
+        if (isDead()==false) {
+            int damage = (int) (Math.random() * getAttack());
+
+            //クリティカルダメージのため
+            if( Math.random()<0.3){
+                damage=damage*2;
+                critical=true;
+            }else{
+                critical=false;
+            }
+
+            if(damage==0){
+                System.out.printf("%sの攻撃！,,,だが、%sは攻撃を回避した！\n",getName(), opponent.getName(), damage);
+            }else if(critical==true){
+                System.out.printf("%sの攻撃！会心の一撃！！%sに%dのダメージを与えた！！\n",getName(),opponent.getName(),damage);
+            }else{
+                System.out.printf("%sの攻撃！%sに%dのダメージを与えた！！\n",getName(),opponent.getName(),damage);
+            }
+            opponent.wounded(damage);
+        }
+    }
     public Hero(String name, int hitPoint,int attack){
         super(name,hitPoint,attack);
     }
+
 }
